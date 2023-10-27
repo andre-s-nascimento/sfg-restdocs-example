254183df7915aa54124f7e6a98371c8c439eeb25
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-27 13:49:06 -0300

    Final Aula 95

--- src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -2,11 +2,13 @@ package guru.springframework.sfgrestdocsexample.web.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -44,11 +46,29 @@ class BeerControllerTest {
   void getBeerById() throws Exception {
     given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-    mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+    mockMvc
+        .perform(
+            get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                .param("iscold", "yes")
+                .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        .andDo(document("v1/beer", pathParameters(
-            parameterWithName("beerId").description("UUID of desired beer to get.")
-        )));
+        .andDo(
+            document(
+                "v1/beer",
+                responseFields(
+                    fieldWithPath("id").description("Id of Beer"),
+                    fieldWithPath("version").description("Version number"),
+                    fieldWithPath("createdDate").description("Date Created"),
+                    fieldWithPath("lastModifiedDate").description("Date Updated"),
+                    fieldWithPath("beerName").description("Beer Name"),
+                    fieldWithPath("beerStyle").description("Beer Style"),
+                    fieldWithPath("upc").description("UPC of Beer"),
+                    fieldWithPath("price").description("Price"),
+                    fieldWithPath("quantityOnHand").description("Quantity On hand")),
+                queryParameters(
+                    parameterWithName("iscold").description("Is Beer Cold Query param.")),
+                pathParameters(
+                    parameterWithName("beerId").description("UUID of desired beer to get."))));
   }
 
   @Test
@@ -58,7 +78,20 @@ class BeerControllerTest {
 
     mockMvc
         .perform(post("/api/v1/beer").contentType(MediaType.APPLICATION_JSON).content(beerDtoJson))
-        .andExpect(status().isCreated());
+        .andExpect(status().isCreated())
+        .andDo(
+            document(
+                "v1/beer",
+                requestFields(
+                    fieldWithPath("id").ignored(),
+                    fieldWithPath("version").ignored(),
+                    fieldWithPath("createdDate").ignored(),
+                    fieldWithPath("lastModifiedDate").ignored(),
+                    fieldWithPath("beerName").description("Name of the desired beer."),
+                    fieldWithPath("beerStyle").description("Style of the Beer."),
+                    fieldWithPath("upc").description("Beer UPC").attributes(),
+                    fieldWithPath("price").description("Beer Price"),
+                    fieldWithPath("quantityOnHand").ignored())));
   }
 
   @Test
